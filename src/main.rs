3e2988e66f99f4c8bfe40b380155c3640f3be336
secona <77039267+secona@users.noreply.github.com>
2023-10-22 08:16:43 +0700

    feat: add data and some pfps

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,21 @@ use reqwest::header::HeaderMap;
 use serde::Serialize;
 
 #[derive(Serialize)]
-struct Mahasiswa<'a> {
-    id: &'a str,
+struct Mahasiswa {
+    id: String,
     #[serde(rename(serialize = "namaLengkap"))]
-    nama_lengkap: &'a str,
+    nama_lengkap: String,
     #[serde(rename(serialize = "namaPanggilan"))]
-    nama_panggilan: &'a str,
-    jurusan: &'a str,
-    tanggal_lahir: &'a str,
-    linkedin: &'a str,
-    instagram: &'a str,
-    twitter: &'a str,
-    line: &'a str,
-    deskripsi: &'a str,
-    message: &'a str,
-    interests: Vec<&'a str>,
+    nama_panggilan: String,
+    jurusan: String,
+    tanggal_lahir: String,
+    linkedin: String,
+    instagram: String,
+    twitter: String,
+    line: String,
+    deskripsi: String,
+    message: String,
+    interests: Vec<String>,
 }
 
 #[tokio::main]
@@ -50,61 +50,73 @@ async fn main() {
         .await
         .unwrap();
 
-    fs::create_dir("./dist").unwrap();
+    fs::create_dir_all("./data/pfp").unwrap();
     let result = result.text().await.unwrap();
     let mut rdr = csv::Reader::from_reader(result.as_bytes());
-    let mut futs = Vec::new();
+    let mut data: Vec<Mahasiswa> = Vec::new();
 
     for record in rdr.records() {
         let record = record.unwrap();
         let client = Arc::clone(&client);
 
-        futs.push(async move {
-            let mahasiswa = Mahasiswa {
-                id: &record[2].to_string().to_case(Case::Kebab),
-                nama_lengkap: &record[2],
-                nama_panggilan: &record[3],
-                jurusan: &record[6],
-                tanggal_lahir: &record[7],
-                linkedin: &record[8],
-                instagram: &record[9],
-                twitter: &record[10],
-                line: &record[11],
-                deskripsi: &record[12],
-                message: &record[13],
-                interests: vec![&record[14], &record[15], &record[16]],
-            };
+        let id = &record[2].to_string();
+        let id = id.to_case(Case::Kebab);
 
-            let stringified = serde_json::to_string_pretty(&mahasiswa).unwrap();
-            let filename = format!("./dist/{}.json", mahasiswa.id);
-            let mut file = fs::File::create(filename).unwrap();
-            file.write_all(stringified.as_bytes()).unwrap();
+        let mahasiswa = Mahasiswa {
+            id: id.clone(),
+            nama_lengkap: record[2].to_string(),
+            nama_panggilan: record[3].to_string(),
+            jurusan: record[6].to_string(),
+            tanggal_lahir: record[7].to_string(),
+            linkedin: record[8].to_string(),
+            instagram: record[9].to_string(),
+            twitter: record[10].to_string(),
+            line: record[11].to_string(),
+            deskripsi: record[12].to_string(),
+            message: record[13].to_string(),
+            interests: vec![
+                record[14].to_string(),
+                record[15].to_string(),
+                record[16].to_string(),
+            ],
+        };
 
-            let pfp_url = reqwest::Url::parse(&record[5]).unwrap();
-            let pair: HashMap<_, String> = pfp_url.query_pairs().into_owned().collect();
-            let url = format!(
-                "https://www.googleapis.com/drive/v3/files/{}",
-                pair.get("id").unwrap()
-            );
+        data.push(mahasiswa);
 
-            let profile_picture = client
-                .get(url)
-                .query(&[("alt", "media")])
-                .send()
-                .await
-                .unwrap()
-                .bytes()
-                .await
-                .unwrap();
+        let pfp_url = reqwest::Url::parse(&record[5]).unwrap();
+        let pair: HashMap<_, String> = pfp_url.query_pairs().into_owned().collect();
+        let url = format!(
+            "https://www.googleapis.com/drive/v3/files/{}",
+            pair.get("id").unwrap()
+        );
 
-            let img = image::load_from_memory(profile_picture.as_bytes()).unwrap();
-            let img = img.resize(512, 512, image::imageops::FilterType::Lanczos3);
-            let filename = format!("./dist/{}.png", mahasiswa.id);
-            img.save(filename).unwrap();
+        let profile_picture = client
+            .get(url)
+            .query(&[("alt", "media")])
+            .send()
+            .await
+            .unwrap()
+            .bytes()
+            .await
+            .unwrap();
 
-            println!("Saved {}.", mahasiswa.id);
-        })
+        let bytes = profile_picture.as_bytes();
+
+        match image::load_from_memory(bytes) {
+            Ok(img) => {
+                let img = img.resize(512, 512, image::imageops::FilterType::Lanczos3);
+                let filename = format!("./data/pfp/{}.png", id);
+                img.save(filename).unwrap();
+
+                println!("Saved {}.", id);
+            }
+            Err(err) => {
+                println!("Error {}. {}.", id, err.to_string())
+            }
+        }
     }
 
-    futures::future::join_all(futs).await;
+    let stringified = serde_json::to_string_pretty(&data).unwrap();
+    let mut file = fs::File::create("./data/data.json").unwrap();
+    file.write_all(stringified.as_bytes()).unwrap();
 }
